2afc226224825903c6362675385f75246c7a1d3f
yoko <yoko.yuasa@aol.com>
2024-03-21 03:15:16 +0000

    follow, unfollow

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,23 +4,23 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-use Illuminate\Support\Facades\Auth;                        // 追加
-use App\Models\User;                                        // 追加
+use Illuminate\Support\Facades\Auth;                        
+use App\Models\User;                                        
 
 class UsersController extends Controller
 {
-    public function index()                                 // 追加       
-    {                                                       // 追加
+    public function index()                                        
+    {                                                       
         // ユーザ一覧をidの降順で取得
-        $users = User::orderBy('id', 'desc')->paginate(10); // 追加
+        $users = User::orderBy('id', 'desc')->paginate(10); 
 
         // ユーザ一覧ビューでそれを表示
-        return view('users.index', [                        // 追加
-            'users' => $users,                              // 追加
-        ]);                                                 // 追加
-    }                                                       // 追加
+        return view('users.index', [                        
+            'users' => $users,                              
+        ]);                                                 
+    }                                                       
     
-    public function show($id)                               // 追加
+    public function show($id)                               
     {
         // idの値でユーザを検索して取得
         $user = User::findOrFail($id);
@@ -36,5 +36,53 @@ class UsersController extends Controller
             'user' => $user,
             'microposts' => $microposts,
         ]);                                                 
-    }                                                       
+    }
+    
+    /**
+     * ユーザのフォロー一覧ページを表示するアクション。
+     *
+     * @param  $id  ユーザのid
+     * @return \Illuminate\Http\Response
+     */
+    public function followings($id)
+    {
+        // idの値でユーザを検索して取得
+        $user = User::findOrFail($id);
+
+        // 関係するモデルの件数をロード
+        $user->loadRelationshipCounts();
+
+        // ユーザのフォロー一覧を取得
+        $followings = $user->followings()->paginate(10);
+
+        // フォロー一覧ビューでそれらを表示
+        return view('users.followings', [
+            'user' => $user,
+            'users' => $followings,
+        ]);
+    }
+
+    /**
+     * ユーザのフォロワー一覧ページを表示するアクション。
+     *
+     * @param  $id  ユーザのid
+     * @return \Illuminate\Http\Response
+     */
+    public function followers($id)
+    {
+        // idの値でユーザを検索して取得
+        $user = User::findOrFail($id);
+
+        // 関係するモデルの件数をロード
+        $user->loadRelationshipCounts();
+
+        // ユーザのフォロワー一覧を取得
+        $followers = $user->followers()->paginate(10);
+
+        // フォロワー一覧ビューでそれらを表示
+        return view('users.followers', [
+            'user' => $user,
+            'users' => $followers,
+        ]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,9 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\UsersController; // 追記
-use App\Http\Controllers\MicropostsController; //追記
+use App\Http\Controllers\UsersController; 
+use App\Http\Controllers\MicropostsController;
+use App\Http\Controllers\UserFollowController; //追記
 
 /*
 |--------------------------------------------------------------------------
@@ -23,7 +24,14 @@ Route::get('/dashboard', [MicropostsController::class, 'index'])->middleware(['a
 
 require __DIR__.'/auth.php';
 
-Route::group(['middleware' => ['auth']], function () {                                    // 追記
-    Route::resource('users', UsersController::class, ['only' => ['index', 'show']]);     // 追記
+Route::group(['middleware' => ['auth']], function () {    
+    Route::group(['prefix' => 'users/{id}'], function() {  // 追記
+        Route::post('follow', [UserFollowController::class, 'store'])->name('user.follow');  // 追記
+        Route::delete('unfollow', [UserFollowController::class, 'destroy'])->name('user.unfollow');  // 追記
+        Route::get('followings', [UsersController::class, 'followings'])->name('users.followings'); // 追記
+        Route::get('followers', [UsersController::class, 'followers'])->name('users.followers');  // 追記
+    }); // 追記
+
+    Route::resource('users', UsersController::class, ['only' => ['index', 'show']]);     
     Route::resource('microposts', MicropostsController::class, ['only' => ['store', 'destroy']]);
-});                                                                                       // 追記
+});                                                                                       
